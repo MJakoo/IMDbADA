cfb18000f45620077efa8659f3d1d3955c342f2a
Javid Magsudov <97388861+MJakoo@users.noreply.github.com>
2022-12-28 00:39:14 +0400

    Updates

---
+++ precache-manifest.81554eb06dfd6500b5f9a7b088e83622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad29cc7f715e0164358d",
+    "url": "/IMDbADA/static/css/main.0cf5fed7.chunk.css"
+  },
+  {
+    "revision": "ad29cc7f715e0164358d",
+    "url": "/IMDbADA/static/js/main.ad29cc7f.chunk.js"
+  },
+  {
+    "revision": "f56eed5341b57b84354b",
+    "url": "/IMDbADA/static/js/1.f56eed53.chunk.js"
+  },
+  {
+    "revision": "0bcb3c7ac16f89c5d772",
+    "url": "/IMDbADA/static/js/runtime~main.0bcb3c7a.js"
+  },
+  {
+    "revision": "11d1df8d244bbe4e561fd742bfd8f722",
+    "url": "/IMDbADA/static/media/logo-color.11d1df8d.png"
+  },
+  {
+    "revision": "59d61017209de5811767125714ba8d71",
+    "url": "/IMDbADA/index.html"
+  }
+];
